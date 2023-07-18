b1e80944e7d91997f7baf899f03032ac126b4c38
Randy420Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-07-18 07:50:28 +0300

    added python download script again

--- get-python.sh
+++ get-python.sh
@@ -1,3 +1,4 @@
 #!bin/bash
 
-time wget https://github.com/Randy420Marsh/cpython/releases/download/v3.10.12/Python-3.10.12.tar.xz
+wget https://github.com/Randy420Marsh/cpython/releases/download/v3.10.12/Python-3.10.12.tar.xz && \
+tar -xf Python-3.10.12.tar.xz
