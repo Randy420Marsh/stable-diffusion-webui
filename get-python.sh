87d2d0e62aa9995c3088a047ccb00a0438a3ad0e
Randy420Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-07-25 08:00:57 +0300

    checkout main

--- get-python.sh
+++ get-python.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 wget https://github.com/Randy420Marsh/cpython/releases/download/v3.10.12/Python-3.10.12.tar.xz && \
-tar -xf Python-3.10.12.tar.xz
+tar -xf Python-3.10.12.tar.xz && \
+./activate.sh && \
+python -m pip install --upgrade pip
