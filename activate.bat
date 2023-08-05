a79555a61eefcb3f8419ced6189a32f7a277d4ab
Randy420Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-08-05 04:11:25 +0300

    update

--- activate.bat
+++ activate.bat
@@ -8,4 +8,4 @@ setlocal enabledelayedexpansion
 
 set "python=%current_path%\Python-3.10.12\python"
 
-IF exist ./venv (cmd /k call .\venv\scripts\activate.bat)  ELSE (cmd /k .\Python-3.10.12\python -m venv venv && cmd /k call .\venv\scripts\activate.bat)
+IF exist ./venv (cmd /k call %current_path%\venv\scripts\activate.bat)  ELSE (cmd /k %current_path%\Python-3.10.12\python -m venv venv && cmd /k call .%current_path%\venv\scripts\activate.bat)
