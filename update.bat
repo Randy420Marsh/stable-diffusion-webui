490159c4869ad223e5f871b75666930fcf2c0d13
Randy420Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-08-05 06:07:08 +0300

    oops

--- update.bat
+++ update.bat
@@ -34,62 +34,44 @@ pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
 
 pip install xformers
 
-cd %EXTENSIONS_DIR%
-IF exist sd-webui-llul (cd %EXTENSIONS_DIR%\sd-webui-llul && git pull) ELSE (git clone https://github.com/Randy420Marsh/sd-webui-llul.git)
-
 cd %EXTRENSIONS_DIR%
-IF exist SD-latent-mirroring (cd %EXTENSIONS_DIR%\SD-latent-mirroring && git pull) ELSE (git clone https://github.com/Randy420Marsh/SD-latent-mirroring.git)
-
+IF exist sd-webui-llul (cd %EXTENSIONS_DIR%\sd-webui-llul && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/sd-webui-llul.git)
 cd %EXTRENSIONS_DIR%
-IF exist a1111-sd-webui-haku-img (cd %EXTENSIONS_DIR%\a1111-sd-webui-haku-img && git pull) ELSE (git clone https://github.com/Randy420Marsh/a1111-sd-webui-haku-img.git)
-
+IF exist SD-latent-mirroring (cd %EXTENSIONS_DIR%\SD-latent-mirroring && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/SD-latent-mirroring.git)
 cd %EXTRENSIONS_DIR%
-IF exist a1111-sd-webui-lycoris (cd %EXTENSIONS_DIR%\a1111-sd-webui-lycoris && git pull) ELSE (git clone https://github.com/Randy420Marsh/a1111-sd-webui-lycoris.git)
-
+IF exist a1111-sd-webui-haku-img (cd %EXTENSIONS_DIR%\a1111-sd-webui-haku-img && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/a1111-sd-webui-haku-img.git)
 cd %EXTRENSIONS_DIR%
-IF exist batch-face-swap (cd %EXTENSIONS_DIR%\batch-face-swap && git pull) ELSE (git clone https://github.com/Randy420Marsh/batch-face-swap.git)
-
+IF exist a1111-sd-webui-lycoris (cd %EXTENSIONS_DIR%\a1111-sd-webui-lycoris && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/a1111-sd-webui-lycoris.git)
 cd %EXTRENSIONS_DIR%
-IF exist gif2gif (cd %EXTENSIONS_DIR%\gif2gif && git pull) ELSE (git clone https://github.com/Randy420Marsh/gif2gif.git)
-
+IF exist batch-face-swap (cd %EXTENSIONS_DIR%\batch-face-swap && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/batch-face-swap.git)
 cd %EXTRENSIONS_DIR%
-IF exist multi-subject-render (cd %EXTENSIONS_DIR%\multi-subject-render && git pull) ELSE (git clone https://github.com/Randy420Marsh/multi-subject-render.git)
-
+IF exist gif2gif (cd %EXTENSIONS_DIR%\gif2gif && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/gif2gif.git)
 cd %EXTRENSIONS_DIR%
-IF exist openOutpaint-webUI-extension (cd %EXTENSIONS_DIR%\openOutpaint-webUI-extension && git pull) ELSE (git clone https://github.com/Randy420Marsh/openOutpaint-webUI-extension.git)
-
+IF exist multi-subject-render (cd %EXTENSIONS_DIR%\multi-subject-render && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/multi-subject-render.git)
 cd %EXTRENSIONS_DIR%
-IF exist sd-dynamic-thresholding (cd %EXTENSIONS_DIR%\sd-dynamic-thresholding && git pull) ELSE (git clone https://github.com/Randy420Marsh/sd-dynamic-thresholding.git)
-
+IF exist openOutpaint-webUI-extension (cd %EXTENSIONS_DIR%\openOutpaint-webUI-extension && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/openOutpaint-webUI-extension.git)
 cd %EXTRENSIONS_DIR%
-IF exist sd-extension-steps-animation (cd %EXTENSIONS_DIR%\sd-extension-steps-animation && git pull) ELSE (git clone https://github.com/Randy420Marsh/sd-extension-steps-animation.git)
-
+IF exist sd-dynamic-thresholding (cd %EXTENSIONS_DIR%\sd-dynamic-thresholding && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/sd-dynamic-thresholding.git)
 cd %EXTRENSIONS_DIR%
-IF exist sd-webui-3d-open-pose-editor (cd %EXTENSIONS_DIR%\sd-webui-3d-open-pose-editor && git pull) ELSE (git clone https://github.com/Randy420Marsh/sd-webui-3d-open-pose-editor.git)
-
+IF exist sd-extension-steps-animation (cd %EXTENSIONS_DIR%\sd-extension-steps-animation && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/sd-extension-steps-animation.git)
 cd %EXTRENSIONS_DIR%
-IF exist sd-webui-ar (cd %EXTENSIONS_DIR%\sd-webui-ar && git pull) ELSE (git clone https://github.com/Randy420Marsh/sd-webui-ar.git)
-
+IF exist sd-webui-3d-open-pose-editor (cd %EXTENSIONS_DIR%\sd-webui-3d-open-pose-editor && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/sd-webui-3d-open-pose-editor.git)
 cd %EXTRENSIONS_DIR%
-IF exist sd-webui-controlnet (cd %EXTENSIONS_DIR%\sd-webui-controlnet && git pull) ELSE (git clone https://github.com/Randy420Marsh/sd-webui-controlnet.git)
-
+IF exist sd-webui-ar (cd %EXTENSIONS_DIR%\sd-webui-ar && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/sd-webui-ar.git)
 cd %EXTRENSIONS_DIR%
-IF exist sd-webui-model-converter (cd %EXTENSIONS_DIR%\sd-webui-model-converter && git pull) ELSE (git clone https://github.com/Randy420Marsh/sd-webui-model-converter.git)
-
+IF exist sd-webui-controlnet (cd %EXTENSIONS_DIR%\sd-webui-controlnet && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/sd-webui-controlnet.git)
 cd %EXTRENSIONS_DIR%
-IF exist sd_save_intermediate_images (cd %EXTENSIONS_DIR%\sd_save_intermediate_images && git pull) ELSE (git clone https://github.com/Randy420Marsh/sd_save_intermediate_images.git)
-
+IF exist sd-webui-model-converter (cd %EXTENSIONS_DIR%\sd-webui-model-converter && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/sd-webui-model-converter.git)
 cd %EXTRENSIONS_DIR%
-IF exist stable-diffusion-webui-rembg (cd %EXTENSIONS_DIR%\stable-diffusion-webui-rembg && git pull) ELSE (git clone https://github.com/Randy420Marsh/stable-diffusion-webui-rembg.git)
-
+IF exist sd_save_intermediate_images (cd %EXTENSIONS_DIR%\sd_save_intermediate_images && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/sd_save_intermediate_images.git)
 cd %EXTRENSIONS_DIR%
-IF exist stable-diffusion-webui-sonar(cd %EXTENSIONS_DIR%\stable-diffusion-webui-sonar && git pull) ELSE (git clone https://github.com/Randy420Marsh/stable-diffusion-webui-sonar.git)
-
+IF exist stable-diffusion-webui-rembg (cd %EXTENSIONS_DIR%\stable-diffusion-webui-rembg && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/stable-diffusion-webui-rembg.git)
 cd %EXTRENSIONS_DIR%
-IF exist ultimate-upscale-for-automatic1111 (cd %EXTENSIONS_DIR%\ultimate-upscale-for-automatic1111 && git pull) ELSE (git clone https://github.com/Randy420Marsh/ultimate-upscale-for-automatic1111.git)
-
+IF exist stable-diffusion-webui-sonar(cd %EXTENSIONS_DIR%\stable-diffusion-webui-sonar && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/stable-diffusion-webui-sonar.git)
 cd %EXTRENSIONS_DIR%
-IF exist video_loopback_for_webui (cd %EXTENSIONS_DIR%\video_loopback_for_webui && git pull) ELSE (git clone https://github.com/Randy420Marsh/video_loopback_for_webui.git)
+IF exist ultimate-upscale-for-automatic1111 (cd %EXTENSIONS_DIR%\ultimate-upscale-for-automatic1111 && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/ultimate-upscale-for-automatic1111.git)
+cd %EXTRENSIONS_DIR%
+IF exist video_loopback_for_webui (cd %EXTENSIONS_DIR%\video_loopback_for_webui && git pull) ELSE (cd %EXTRENSIONS_DIR% && git clone https://github.com/Randy420Marsh/video_loopback_for_webui.git)
 
 cd %AUTOMATIC1111_DIR%
 
