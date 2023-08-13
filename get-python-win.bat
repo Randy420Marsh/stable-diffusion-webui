84f0eeb44d9aeadd394052240db35f20ef822145
Randy Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-08-13 07:05:31 +0300

    Update get-python-win.bat

--- get-python-win.bat
+++ get-python-win.bat
@@ -8,14 +8,10 @@ set "extracted_folder=Python-3.10.12"
 
 REM Check if the file already exists
 if exist "%filename%" (
-    echo File already exists. Exiting...
-    exit /b
+    echo python already exists. Exiting...
 )
 
 REM If the file does not exist, download it first
-echo Downloading the file...
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%url%', '%filename%')"
-
-REM Extract the downloaded file
-echo Extracting the downloaded file...
-tar -xf "%filename%" -C "%extracted_folder%"
+echo Downloading python...
+curl -LO %url%
+cmd /k tar -xf "%filename%" "%extracted_folder%"
