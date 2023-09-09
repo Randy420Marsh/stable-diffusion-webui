3a8aeaf0c5a535fe10ceaf264f68bf91557b76a6
Randy420Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-09-09 16:47:25 +0300

    my first commit

--- webui-user.bat
+++ webui-user.bat
@@ -2,9 +2,11 @@
 
 set "SD_ROOT_PATH=C://AI//stable_diffusion_models_and_vae"
 
-set PYTHON=
+set "PYTHON=C://Python-3.10//PCbuild//amd64//python.exe"
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=--controlnet-dir "%SD_ROOT_PATH%//models//ControlNet" --controlnet-annotator-models-path "%SD_ROOT_PATH%//models//ControlNet//annotator//models" --lora-dir "%SD_ROOT_PATH%//models//Lora" --gfpgan-dir "%SD_ROOT_PATH%//models//GFPGAN" --vae-dir "%SD_ROOT_PATH%//models//VAE" --ckpt-dir "%SD_ROOT_PATH%//models//" --embeddings-dir "%SD_ROOT_PATH%//models//embeddings" --codeformer-models-path "%SD_ROOT_PATH%//models//Codeformer" --gfpgan-models-path "%SD_ROOT_PATH%//models//GFPGAN" --esrgan-models-path "%SD_ROOT_PATH%//models//ESRGAN" --bsrgan-models-path "%SD_ROOT_PATH%//models//ESRGAN" --realesrgan-models-path "%SD_ROOT_PATH%//models//RealESRGAN" --no-download-sd-model --port 4433 --opt-split-attention --medvram --theme=dark --precision autocast --api
+set VENV_DIR=venv
+set COMMANDLINE_ARGS= --lora-dir "%SD_ROOT_PATH%//models//Lora" --gfpgan-dir "%SD_ROOT_PATH%//models//GFPGAN" --vae-dir "%SD_ROOT_PATH%//models//VAE" --ckpt-dir "%SD_ROOT_PATH%//models//" --embeddings-dir "%SD_ROOT_PATH%//models//embeddings" --codeformer-models-path "%SD_ROOT_PATH%//models//Codeformer" --gfpgan-models-path "%SD_ROOT_PATH%//models//GFPGAN" --esrgan-models-path "%SD_ROOT_PATH%//models//ESRGAN" --bsrgan-models-path "%SD_ROOT_PATH%//models//ESRGAN" --realesrgan-models-path "%SD_ROOT_PATH%//models//RealESRGAN" --no-download-sd-model --port 4433 --no-half-vae --opt-sdp-attention --medvram --theme=dark --precision autocast --api
+
+REM deprecated: --controlnet-dir "%SD_ROOT_PATH%//models//ControlNet" --controlnet-annotator-models-path "%SD_ROOT_PATH%//models//ControlNet//annotator//models"
 
 call webui.bat
