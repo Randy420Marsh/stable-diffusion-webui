42e6d47227f5ade2033cb3384fb58899d1ca220b
Randy Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2024-05-22 00:46:46 +0300

    add civitai extension to the list

--- download-extensions.bat
+++ download-extensions.bat
@@ -39,3 +39,5 @@ git clone https://github.com/Randy420Marsh/ultimate-upscale-for-automatic1111.gi
 git clone https://github.com/Randy420Marsh/video_loopback_for_webui.git
 
 git clone https://github.com/Randy420Marsh/sd-webui-model-converter.git
+
+git clone https://github.com/Randy420Marsh/sd_civitai_extension.git
