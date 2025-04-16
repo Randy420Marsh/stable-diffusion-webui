9ee0a1df16f5b6a37dd4a069a9f546cd7d7bedfa
Randy420Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2025-04-16 20:44:32 +0300

    update scripts

---
+++ start_with_update.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set BNB_CUDA_VERSION=118
+
+set "SD_ROOT_PATH=H://AI//stable_diffusion_models_and_vae"
+
+set "PYTHON=python"
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS= --controlnet-dir "%SD_ROOT_PATH%//models//ControlNet" --controlnet-annotator-models-path "%SD_ROOT_PATH%//models//ControlNet//annotator//models" --lora-dir "%SD_ROOT_PATH%//models//Lora" --gfpgan-dir "%SD_ROOT_PATH%//models//GFPGAN" --vae-dir "%SD_ROOT_PATH%//models//VAE" --ckpt-dir "%SD_ROOT_PATH%//models//" --embeddings-dir "%SD_ROOT_PATH%//models//embeddings" --codeformer-models-path "%SD_ROOT_PATH%//models//Codeformer" --gfpgan-models-path "%SD_ROOT_PATH%//models//GFPGAN" --esrgan-models-path "%SD_ROOT_PATH%//models//ESRGAN" --bsrgan-models-path "%SD_ROOT_PATH%//models//ESRGAN" --realesrgan-models-path "%SD_ROOT_PATH%//models//RealESRGAN" --no-download-sd-model --port 4433 --theme=dark --precision autocast --opt-split-attention --medvram --api --loglevel ERROR
+
+call webui.bat
+
--- update.bat
+++ update.bat
@@ -1,12 +1,16 @@
 @echo off
 
-echo "This script is updated 2024..."
-
 setlocal enabledelayedexpansion
+
+echo This script is updated 2024...
+
+set BNB_CUDA_VERSION=118
+
+git pull
 
 set python_cmd=python
 
-set AUTOMATIC1111_DIR=%CD%
+set AUTOMATIC1111_DIR=%cd%
 
 echo AUTOMATIC1111_DIR: %AUTOMATIC1111_DIR%
 
@@ -27,23 +31,20 @@ REM Prompt user for choice
 set /p user_choice=Do you want to update the GPU or CPU version (G/C)? 
 if /I "%user_choice%"=="G" (
     set venv_dir=venv
-    set install_cmd=pip install "torch==2.1.0" "torchvision==0.16.0" --index-url https://download.pytorch.org/whl/cu118
-    echo "Disabling xformers install because of dependency problems..."
-    set install_xformers=false
+    set install_cmd=pip install "torch==2.1.2+cu118" "torchvision==0.16.2+cu118" --index-url https://download.pytorch.org/whl/cu118
+    set install_xformers=true
 ) else if /I "%user_choice%"=="C" (
     set venv_dir=venv-cpu
-    set install_cmd=pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+    set install_cmd=pip install "torch==2.1.2" "torchvision==0.16.2" --index-url https://download.pytorch.org/whl/cpu
     set install_xformers=false
 ) else (
-    echo "Invalid choice. Please enter G for GPU version or C for CPU version."
+    echo Invalid choice. Please enter G for GPU version or C for CPU version.
     goto choice
 )
 
 %python_cmd% --version
 
 pause
-
-git pull
 
 REM Create and activate virtual environment
 if exist %venv_dir%\Scripts\activate (
@@ -57,29 +58,30 @@ if exist %venv_dir%\Scripts\activate (
 
 cd %AUTOMATIC1111_DIR%
 
-pip uninstall -y torch torchvision xformers torchaudio
-
+pip uninstall -y torch torchvision
 REM Install chosen version of torch and torchvision
 %install_cmd%
 
 pip install -r requirements.txt
+pip install -r requirements_versions.txt
 
 if "%install_xformers%"=="true" (
-    pip install xformers
+    pip install "xformers==0.0.23.post1" --index-url https://download.pytorch.org/whl/cu118
 )
 
 cd %EXTENSIONS_DIR%
 dir
 echo We should now be inside the extensions directory...
 
+REM deprecated
+REM https://github.com/Randy420Marsh/batch-face-swap.git
+
 REM Iterate through each repository URL directly
 for %%i in (
-    ::https://github.com/Randy420Marsh/adetailer.git
     https://github.com/Randy420Marsh/sd-webui-llul.git
     https://github.com/Randy420Marsh/SD-latent-mirroring.git
     https://github.com/Randy420Marsh/a1111-sd-webui-haku-img.git
     https://github.com/Randy420Marsh/sd-webui-stablesr.git
-    https://github.com/Randy420Marsh/batch-face-swap.git
     https://github.com/Randy420Marsh/gif2gif.git
     https://github.com/Randy420Marsh/multi-subject-render.git
     https://github.com/Randy420Marsh/openOutpaint-webUI-extension.git
@@ -95,12 +97,19 @@ for %%i in (
     https://github.com/Randy420Marsh/ultimate-upscale-for-automatic1111.git
     https://github.com/Randy420Marsh/video_loopback_for_webui.git
     https://github.com/Randy420Marsh/multidiffusion-upscaler-for-automatic1111.git
-    ::https://github.com/Randy420Marsh/sd_civitai_extension.git
+    https://github.com/Randy420Marsh/video_loopback_for_webui.git
+    https://github.com/Randy420Marsh/sd-webui-model-converter.git
     https://github.com/Randy420Marsh/stable-diffusion-webui-dataset-tag-editor.git
-    ::https://github.com/Randy420Marsh/webui-stability-api.git
     https://github.com/Randy420Marsh/stable-diffusion-webui-aesthetic-image-scorer.git
+    https://github.com/Randy420Marsh/sd_dreambooth_extension.git
+    https://github.com/Randy420Marsh/sd-webui-gelbooru-prompt.git
+    https://github.com/Randy420Marsh/sd-webui-nsfw-filter.git
+    https://github.com/Randy420Marsh/a1111-sd-webui-tagcomplete.git
+    https://github.com/Randy420Marsh/model-keyword.git
+    https://github.com/Randy420Marsh/sd-dynamic-prompts.git
 ) do (
     set "repo=%%i"
+    setlocal enabledelayedexpansion
     set "repo_name=!repo:~33,-4!"  REM Extract a simple repo name from the URL
     if exist "!repo_name!" (
         echo Updating !repo_name!...
@@ -111,30 +120,67 @@ for %%i in (
         echo Cloning !repo_name!...
         git clone "!repo!" "!repo_name!"
     )
+    endlocal
 )
 
 cd %AUTOMATIC1111_DIR%
 
-pip install fastapi -U
+IF exist ./wildcards (cd wildcards && git reset --hard && git pull && echo "Updated wildcards") ELSE (git clone https://github.com/Randy420Marsh/WC-SDVN.git wildcards && echo "Downloaded/Installed wildcards")
 
-pip install albumentations -U
+cd %AUTOMATIC1111_DIR%
 
-pip uninstall -y watchdog opencv-python-headless
-
-REM Install rembg dependencies
 if exist .\extensions\stable-diffusion-webui-rembg (
-    echo Installing rembg dependencies
-    REM pip install opencv-python-headless==4.6.0.66 watchdog==2.1.9 rembg==2.0.50 onnxruntime pymatting pooch
-    pip install opencv-python-headless>=4.9.0.80 watchdog==2.1.9 rembg==2.0.50 onnxruntime pymatting pooch
+    pip uninstall -y watchdog opencv-python-headless
+    pip install "opencv-python-headless>=4.9.0" "watchdog==2.1.9" "rembg==2.0.50" "onnx==1.17.0" pymatting pooch
 ) else (
-    echo Nothing to do; rembg does not exist...
+    echo Nothing to do rembg does not exist...
+)
+
+REM pip install "opencv-python-headless==4.6.0.66" "watchdog==2.1.9" "rembg==2.0.50" "onnx==1.17.0" pymatting pooch
+
+if exist .\extensions\sd_dreambooth_extension (
+    pip uninstall -y watchdog opencv-python-headless
+    pip install -r .\extensions\sd_dreambooth_extension\requirements.txt
+) else (
+    echo Nothing to do sd_dreambooth_extension does not exist...
+)
+
+
+if exist .\extensions\sd-dynamic-prompts (
+    pip install -r .\extensions\sd-dynamic-prompts\requirements.txt
+) else (
+    echo Nothing to do sd-dynamic-prompts does not exist...
+)
+
+if exist .\extensions\a1111-sd-webui-haku-img (
+    pip install -r .\extensions\a1111-sd-webui-haku-img\requirements.txt
+) else (
+    echo Nothing to do sd-dynamic-prompts does not exist...
 )
 
 echo Fixing dependencies...
 
-REM Deprecated: pip install "opencv-python-headless>=4.9.0" "albumentations==1.4.3"
+pip install "watchdog==2.1.9" "rembg==2.0.50" "pymatting" "pooch" "albumentations==1.4.3" "opencv-python-headless>=4.9.0" "open-clip-torch==2.24.0" "scikit-learn-intelex" "numpy<2.0.0,>=1.0.0" "thinc" "pypiwin32" "openai-clip" "protobuf<5,>=4.25.3" "picologging"
+
+pip install onnxruntime-gpu[cuda,cudnn] --index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-11/pypi/simple/
+
+REM onnx instructions: https://onnxruntime.ai/docs/install/
+
+REM "onnx==1.17.0"
+
+REM "protobuf==5.29.3"
+
+echo "Copy python libs to venv"
+
+mkdir "%AUTOMATIC1111_DIR%\venv\Scripts\libs\"
+
+IF exist "C:\Program Files\Python310\libs\" (copy "C:\Program Files\Python310\libs\*" "%COMFY_UI_DIR%\venv\Scripts\libs\") ELSE (copy "C:\Users\John\AppData\Local\Programs\Python\Python310\libs\*" "%COMFY_UI_DIR%\venv\Scripts\libs\")
 
 echo Update/install finished...
 
-pause
+echo Run accelerate config...
+
+accelerate config
+
 endlocal
+pause
